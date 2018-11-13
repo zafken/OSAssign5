d1e102d116b6092e0f8bcfc5ca27cb1ed4874de4
Walter, Sean Alexandre - SDSU Student <sean.walter@jacks.sdstate.edu>
2018-11-13 14:12:06 -0600

    part 1 update

--- part1.c
+++ part1.c
@@ -1,1 +1,23 @@
 //part 1
+
+int *y = NULL;
+
+
+
+void main()
+{
+	int MAX = 50;
+	int j = 0;
+	int x;
+	y = (int*)malloc(MAX*sizeof(int));
+
+	for (j = 0; j < MAX; j++)
+	{
+		printf("Enter an integer value: ");
+		scanf("%d", &x);
+		if (x == 0)
+			y[j] = 0;
+		else
+			y[j] = 10;
+	}
+}
